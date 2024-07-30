04372cc75903d6f761df38bd2f142b4d15eca117
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-07-30 08:32:44 +0530

    fixing bug: updated the url of api

--- src/App.js
+++ src/App.js
@@ -342,7 +342,7 @@ function MovieDetails({
     function () {
       async function fetchDetails() {
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
         );
         const data = await res.json();
         // console.log(data);
